648bc16008b1ea3a0401916c9fff513ca06d3c93
Shashwat Dixit <shashwatmain@gmail.com>
2023-09-11 00:57:55 +0530

    Checkup...

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const FaqPage: NextPage = () => {
+    return(
+        <div>
+            <h1 className="text-5xl flex justify-content">FAQ Page</h1>
+        </div>
+    )
+}
+
+export default FaqPage;
